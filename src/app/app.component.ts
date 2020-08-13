0a8b2dd406c9d5daaffef0b5f0aab700466b24d9
caudlejohnp <caudlejohnp@gmail.com>
2020-08-12 20:32:24 -0500

    added addTodo( )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,23 @@ export class AppComponent implements OnInit {
   title = "Todo List";
   todoList: any[] = [];
   todoTitle = "";
+
   ngOnInit(): void {
+    this.todoTitle = "";
     this.todoList = [{ title: "Install Angular CLI", isDone: false }];
   }
 
   deleteTodo(todo: any) {
     const index = this.todoList.findIndex((todoItem) => todoItem === todo);
     this.todoList.splice(index, 1);
+  }
+
+  addTodo(): void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false,
+    });
+
+    this.todoTitle = "";
   }
 }
