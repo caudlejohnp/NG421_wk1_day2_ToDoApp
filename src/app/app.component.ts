3d8115b36a61ba4b46a47713626d35453305688e
caudlejohnp <caudlejohnp@gmail.com>
2020-08-12 20:21:26 -0500

    added input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+export class AppComponent implements OnInit {
+  title = "Todo List";
+  todoList: any[] = [];
+  todoTitle = "";
+  ngOnInit(): void {
+    this.todoList = [{ title: "Install Angular CLI", isDone: false }];
+  }
+
+  deleteTodo(todo: any) {
+    const index = this.todoList.findIndex((todoItem) => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
 }
